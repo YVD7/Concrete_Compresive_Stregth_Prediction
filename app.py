5401998cae64f3824ead8dcc633e38736dc25eb8
YVD7 <yashdawande7@gmail.com>
2022-10-04 14:54:31 +0530

    logging and exception

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from crypt import methods
+import sys
 from flask import Flask
-from importlib_metadata import method_cache
+from Main.logger import logging
+from Main.exception import MainException
+
 
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POSt"])
 def index():
-    return "Starting Machine Learning Project"
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        Main = MainException(e,sys)
+        logging.info(Main.error_message)
+        logging.info("We are testing logging module")
+
+    return "CI CD pipeline has established."
+
 
 
 if __name__=="__main__":
